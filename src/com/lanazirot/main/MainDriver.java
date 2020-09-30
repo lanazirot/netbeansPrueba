f5f85d67e0d23261b43315de8b8e70a16dafc191
Alan Peña <lanazirot@gmail.com>
2020-09-29 19:22:11 -0500

    First commit message

---
+++ src/com/lanazirot/main/MainDriver.java
@@ -0,0 +1,22 @@
+package com.lanazirot.main;
+
+/**
+ *
+ * @author alanc
+ * @see lol
+ */
+
+    record Persona(String nombre, int edad){
+        public void printSomething(){
+            System.out.println("Algo");
+        }
+    }
+
+
+public class MainDriver {
+    public static void main(String[] args) {
+        var per = new Persona("Alan",14);
+        System.out.println(per);
+        
+    }
+}
